6aafc2eb81e18c0b803bb409b80684cdc68e3f8d
FiddyOnFiddy <matthewbiggins@hotmail.co.uk>
2019-04-20 16:31:19 +0100

    Fixed box pushing

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,13 +14,6 @@ public class PlayerScript : MonoBehaviour
     public Vector3 boxDesiredPosition;
     public GameObject box;
 
-    public Vector2 dir;
-
-    public float left = -1.0f;
-    public float right = 1.0f;
-    public float up = 1.0f;
-    public float down = -1.0f;
-
     public Rigidbody2D rb;
 
 
@@ -34,25 +27,18 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         PlayerMovement();
-        BoxMovement();
-
-        //transform.position = Vector3.Lerp(transform.position, desiredPosition, interpolationSpeed * Time.deltaTime);
-
-        rb.MovePosition(Vector3.Lerp(transform.position, desiredPosition, interpolationSpeed * Time.deltaTime));
-
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, interpolationSpeed * Time.deltaTime);
         
-
-       // Debug.Log(dir);
 
         if(box != null)
         {
-
-            dir = (gameObject.transform.position - box.transform.position).normalized;
-
             box.transform.position = Vector3.Lerp(box.transform.position, boxDesiredPosition, boxInterpolationSpeed * Time.deltaTime);
-
-
         }
+
+        Mathf.Round(boxDesiredPosition.x);
+        Mathf.Round(boxDesiredPosition.y);
+
+        
 
 
     }
@@ -66,17 +52,17 @@ public class PlayerScript : MonoBehaviour
                 desiredPosition += Vector3.right;
                 timestamp = Time.time + timeBetweenMoves;
             }
-            if (Input.GetAxisRaw("Horizontal") < 0.0f)
+            else if (Input.GetAxisRaw("Horizontal") < 0.0f)
             {
                 desiredPosition += Vector3.left;
                 timestamp = Time.time + timeBetweenMoves;
             }
-            if (Input.GetAxisRaw("Vertical") > 0.0f)
+            else if (Input.GetAxisRaw("Vertical") > 0.0f)
             {
                 desiredPosition += Vector3.up;
                 timestamp = Time.time + timeBetweenMoves;
             }
-            if (Input.GetAxisRaw("Vertical") < 0.0f)
+            else if (Input.GetAxisRaw("Vertical") < 0.0f)
             {
                 desiredPosition += Vector3.down;
                 timestamp = Time.time + timeBetweenMoves;
@@ -84,99 +70,31 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
-    void BoxMovement()
-    {
-        /* 
-         *RaycastHit2D hitRight = Physics2D.Raycast(gameObject.transform.position, Vector2.right, 0.5f);
-         Debug.DrawRay(gameObject.transform.position, Vector3.right / 2, Color.white);
-
-         if(hitRight.collider != null)
-         {
-             if (hitRight.collider.tag == "Box" && Input.GetAxisRaw("Horizontal") > 0.0f)
-             {
-                 boxPosition = hitRight.collider.transform.position;
-                 boxPosition += Vector3.right;
-                 hitRight.collider.transform.position = boxPosition;
-             }
-         }
-
-         RaycastHit2D hitLeft = Physics2D.Raycast(gameObject.transform.position, Vector2.left, 0.5f);
-         Debug.DrawRay(gameObject.transform.position, Vector3.left / 2, Color.white);
-
-         if (hitLeft.collider != null)
-         {
-             if (hitLeft.collider.tag == "Box" && Input.GetAxisRaw("Horizontal") < 0.0f)
-             {
-                 boxPosition = hitLeft.collider.transform.position;
-                 boxPosition += Vector3.left;
-                 hitLeft.collider.transform.position = boxPosition;
-             }
-         }
-
-         RaycastHit2D hitUp = Physics2D.Raycast(gameObject.transform.position, Vector2.up, 0.5f);
-         Debug.DrawRay(gameObject.transform.position, Vector3.up / 2, Color.white);
-
-         if (hitUp.collider != null)
-         {
-             if (hitUp.collider.tag == "Box" && Input.GetAxisRaw("Vertical") > 0.0f)
-             {
-                 boxPosition = hitUp.collider.transform.position;
-                 boxPosition += Vector3.up;
-                 hitUp.collider.transform.position = boxPosition;
-             }
-         }
-
-         RaycastHit2D hitDown = Physics2D.Raycast(gameObject.transform.position, Vector2.down, 0.5f);
-         Debug.DrawRay(gameObject.transform.position, Vector3.down / 2, Color.white);
-
-         if (hitDown.collider != null)
-         {
-             if (hitDown.collider.tag == "Box" && Input.GetAxisRaw("Vertical") < 0.0f)
-             {
-                 boxPosition = hitDown.collider.transform.position;
-                 boxPosition += Vector3.down;
-                 hitDown.collider.transform.position = boxPosition;
-             }
-         }
-         */
-
-
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Box")
         {
-           // dir = (gameObject.transform.position - collision.transform.position).normalized;
-
             boxDesiredPosition = collision.gameObject.transform.position;
             box = collision.gameObject;
-        
-            if (Mathf.Approximately(dir.x, left))
+
+            if (Input.GetAxisRaw("Horizontal") > 0.0f)
             {
                 boxDesiredPosition += Vector3.right;
-                //collision.gameObject.transform.position = boxDesiredPosition;
-                Debug.Log(dir);
             }
-
-            if (Mathf.Approximately(dir.x, right))
+            if (Input.GetAxisRaw("Horizontal") < 0.0f)
             {
                 boxDesiredPosition += Vector3.left;
-                //collision.gameObject.transform.position = boxDesiredPosition;
             }
-            if (Mathf.Approximately(dir.y, up))
-            {
-                boxDesiredPosition += Vector3.down;
-                //collision.gameObject.transform.position = boxDesiredPosition;
-            }
-            if (Mathf.Approximately(dir.y, down))
+            if (Input.GetAxisRaw("Vertical") > 0.0f)
             {
                 boxDesiredPosition += Vector3.up;
-                //collision.gameObject.transform.position = boxDesiredPosition;
             }
-
-
+            if (Input.GetAxisRaw("Vertical") < 0.0f)
+            {
+                boxDesiredPosition += Vector3.down;
+            }
+            Mathf.Round(box.transform.position.x);
+            Mathf.Round(box.transform.position.y);
         }
     }
 }
